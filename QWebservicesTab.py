7b2412f0d86a12e8a0fec147b933391a76c3fa4e
Karol Chojnowski <karolchojnowski95@gmail.com>
2019-03-12 11:33:54 +0100

    Add layout + textbox

--- QWebservicesTab.py
+++ QWebservicesTab.py
@@ -6,5 +6,25 @@ from SqlConnection import DqConnection
 
 
 class Webservices(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, parent = None):
+        super(Webservices, self).__init__(parent)
+        self.layout_init()
+
+    def layout_init(self):
+        self.name = QtWidgets.QLabel('Enter value:')
+        self.textbox = QtWidgets.QLineEdit()
+
+
+        wgroupbox = QtWidgets.QGroupBox('Option')
+        searchpanel = QtWidgets.QBoxLayout(QtWidgets.QBoxLayout.TopToBottom)
+        searchpanel.addWidget(self.name)
+        searchpanel.addWidget(self.textbox)
+        wgroupbox.setLayout(searchpanel)
+
+
+        grid = QtWidgets.QBoxLayout(QtWidgets.QBoxLayout.TopToBottom, self)
+        grid.addWidget(wgroupbox)
+        self.setLayout(grid)
+
+
+
