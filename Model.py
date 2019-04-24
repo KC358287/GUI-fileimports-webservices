7284d2652953879ca48cf420a89e05a359a61ce4
Karol Chojnowski <karolchojnowski95@gmail.com>
2019-04-24 11:56:16 +0200

    model for tableviews

---
+++ Model.py
@@ -0,0 +1,65 @@
+from PyQt5 import QtCore
+
+
+
+class TableModelW(QtCore.QAbstractTableModel):
+    def __init__(self, res, parent = None):
+        super().__init__(parent)
+
+        self.__data = res
+        self.__header = ['AppInstance',' DateCreate',
+                                                   'StatusCode', 'RequestAction',' ErrorCode',
+                                                    'Content']
+
+    def rowCount( self, parent ):
+        return len(self.__data)
+
+    def columnCount( self , parent ):
+        return len(self.__data[0])
+
+    def flags(self, index):
+        if not index.isValid():
+            return None
+        return QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable
+
+    def data ( self , index , role =  QtCore.Qt.DisplayRole):
+        i = index.row()
+        j = index.column()
+        if role == QtCore.Qt.DisplayRole:
+            return '{0}'.format(self.__data[i][j])
+        else:
+            return QtCore.QVariant()
+
+    def headerData(self , section , orientation , role):
+        if role == QtCore.Qt.DisplayRole:
+            if orientation == QtCore.Qt.Horizontal:
+                return self.__header[section]
+            elif orientation == QtCore.Qt.Vertical:
+                return section
+        return None
+
+
+
+class TableModelF(TableModelW):
+    def __init__(self, res, parent = None):
+        super(TableModelF, self).__init__(res)
+        self.__data = res
+        self.__header = ['Partner', 'FileType', 'FileName',
+                                                    'DateCreate','Imported','InfoCode',
+                                                    'Date AKT/DEZ', 'InternalNumber','Wariant',
+                                                    'MSISDN','IMEI','PRODUCENT','MODEL','PESEL',
+                                                    'NIP','REGON','Segment','Imie','Nazwisko','Nazwa Firmy'
+                                                    ]
+
+        self.rowCount(parent)
+        self.columnCount(parent)
+        self.flags(QtCore.QModelIndex())
+        self.data(QtCore.QModelIndex(),QtCore.Qt.DisplayRole)
+
+    def headerData(self, section, orientation, role):
+        if role == QtCore.Qt.DisplayRole:
+            if orientation == QtCore.Qt.Horizontal:
+                return self.__header[section]
+            elif orientation == QtCore.Qt.Vertical:
+                return section
+        return None
